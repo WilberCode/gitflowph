704c734ce48412e4ade417c59cc67399711c3da1
WilberCode <parionahuaraca@gmail.com>
2020-06-24 01:37:08 -0500

    comit develop

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 
 get_header();
 
-get_template_part( 'templates/partials/example' );
-// get_template_part( 'templates/partials/example' );
+get_template_part( 'templates/partials/example' ); 
 
 get_footer();
